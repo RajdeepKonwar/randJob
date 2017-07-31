2b289a692839ec77d317296d055b651b62856728
Rajdeep Konwar <rkonwar@ucsd.edu>
2017-07-31 16:52:19 -0700

    Updates bracketing style

--- randJob.cpp
+++ randJob.cpp
@@ -46,123 +46,123 @@ using namespace boost::filesystem;
 
 int main() {
   //! Sets path to the current directory
-  path  p (boost::filesystem::current_path());
+  path  p ( boost::filesystem::current_path() );
 
   try {
-    if (exists(p) && is_directory(p)) {
+    if( exists( p ) && is_directory( p ) ) {
       //! Variables
-      bool                        newVid  = false;
-      int                         random, i = 1;
-      std::size_t                 found1, found2;
-      std::string                 wanted1(".mp4");
-      std::string                 wanted2(".flv");
-      std::string                 line;
-      FILE                        *fp;
-
-      //! STLs
-      std::vector<path>           allFiles, files;
-      std::vector<path>::iterator iter;
-      std::vector<std::string>    played;
+      bool        newVid  = false;
+      int         random, i = 1;
+      std::size_t found1, found2;
+      std::string wanted1( ".mp4" );
+      std::string wanted2( ".flv" );
+      std::string line;
+      FILE        *fp;
 
       //! libVLC objects
-      libvlc_instance_t           *inst;
-      libvlc_media_player_t       *mp;
-      libvlc_media_t              *m;
+      libvlc_instance_t     *inst;
+      libvlc_media_player_t *mp;
+      libvlc_media_t        *m;
+
+      //! STLs
+      std::vector< path >           allFiles, files;
+      std::vector< path >::iterator iter;
+      std::vector< std::string >    played;
 
       //! Gets all files and stores into a vector from the currect directory
-      copy(directory_iterator(p), directory_iterator(), back_inserter(allFiles));
-      sort(allFiles.begin(), allFiles.end());
+      copy( directory_iterator( p ), directory_iterator(), back_inserter( allFiles ) );
+      sort( allFiles.begin(), allFiles.end() );
 
       //! Push all the video files (containing .mp4 or .flv) into another vector
-      for (iter = allFiles.begin(); iter != allFiles.end(); ++iter) {
-        std::string temp(((*iter).string()));
-        found1  = temp.find(wanted1);
-        found2  = temp.find(wanted2);
+      for( iter = allFiles.begin(); iter != allFiles.end(); ++iter ) {
+        std::string temp( (*iter).string() );
+        found1  = temp.find( wanted1 );
+        found2  = temp.find( wanted2 );
 
-        if (found1 != std::string::npos || found2 != std::string::npos) {
-          files.push_back(*iter);
+        if( found1 != std::string::npos || found2 != std::string::npos ) {
+          files.push_back( *iter );
         }
       }
 
       //! Reads mano.job to get previously played videos
-      std::ifstream inFile("mano.job");
-      if (inFile) {
-        while (std::getline(inFile, line)) {
-          played.push_back(line);
+      std::ifstream inFile( "mano.job" );
+      if( inFile ) {
+        while( std::getline( inFile, line ) ) {
+          played.push_back( line );
         }
 
         inFile.close();
       }
 
       //! Exits program when all videos watched!
-      if (played.size() == files.size()) {
+      if( played.size() == files.size() ) {
         std::cout << "No new video found! Delete mano.job\n";
-        exit(EXIT_FAILURE);
+        exit( EXIT_FAILURE );
       }
 
       do {
         //! Picks a random video
-        srand((unsigned)time(NULL));
+        srand( (unsigned)time( NULL ) );
         random = rand() % files.size();
-        path f = files.at(random);
+        path f = files.at( random );
 
         //! Repicks if file does not contain .mp4 and .flv
         std::string ext{f.extension().string()};
-        if (ext != ".mp4" && ext != ".flv")
+        if( ext != ".mp4" && ext != ".flv" )
           continue;
 
-        std::string randFile(f.string());
+        std::string randFile( f.string() );
         //! Breaks loop if randomly picked video hasn't been played previously
-        if (std::find(played.begin(), played.end(), randFile) == played.end())
+        if( std::find(played.begin(), played.end(), randFile) == played.end() )
           newVid  = true;
-      } while (newVid == false);
+      } while( newVid == false );
 
-      fp  = std::fopen("mano.job", "a+");
-      if (!fp) {
+      fp  = std::fopen( "mano.job", "a+" );
+      if( !fp ) {
         std::cout << "Cannot open mano.job in append mode!\n";
-        exit(EXIT_FAILURE);
+        exit( EXIT_FAILURE );
       }
 
       //! Adds the new video to list of played videos
-      fprintf(fp, "%s\n", files.at(random).c_str());
-      fclose(fp);
+      fprintf( fp, "%s\n", files.at( random ).c_str() );
+      fclose( fp );
 
       //! Video info
-      std::cout << "Now playing: " << files.at(random).c_str() << std::endl;
+      std::cout << "Now playing: " << files.at( random ).c_str() << std::endl;
 
       //! Loads the VLC engine
-      inst  = libvlc_new (0, NULL);
+      inst  = libvlc_new( 0, NULL );
 
       //! Creates a new item
-      m   = libvlc_media_new_path(inst, files.at(random).c_str());
+      m   = libvlc_media_new_path( inst, files.at( random ).c_str() );
 
       //! Creates a media player playing environement
-      mp  = libvlc_media_player_new_from_media (m);
+      mp  = libvlc_media_player_new_from_media( m );
 
       //! No need to keep the media now
-      libvlc_media_release (m);
+      libvlc_media_release( m );
 
       //! plays the media player
-      libvlc_media_player_play (mp);
+      libvlc_media_player_play( mp );
 
       //! Infinite loop to keep media player running
       do {
         ;
-      } while (i > 0);
+      } while( i > 0 );
 
       //! Stops playing
-      libvlc_media_player_stop (mp);
+      libvlc_media_player_stop( mp );
 
       //! Frees the media_player
-      libvlc_media_player_release (mp);
+      libvlc_media_player_release( mp );
 
-      libvlc_release (inst);
+      libvlc_release( inst );
     }
     else
       std::cout << p << " does not exist\n";
   }
 
-  catch (const filesystem_error& ex) {
+  catch( const filesystem_error& ex ) {
     std::cout << ex.what() << std::endl;;
   }
 
